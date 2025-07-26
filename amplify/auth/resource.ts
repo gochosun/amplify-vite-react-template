2902184a4bd1a86dee1dd19a4a54ad9f5ede80f2
Mj <mj@Mjui-MacBookPro.local>
2025-07-26 22:49:10 +0900

    name add

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -12,6 +12,7 @@ export const auth = defineAuth({
     nickname: {
       required: true,   // 회원가입 시 반드시 입력
       mutable: true     // 가입 후 수정 가능
+      attributeDataType: 'String'
     }
   }
 });
