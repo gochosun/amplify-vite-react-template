32c6bcc6d9df218ffe2e3cb57dd55b5ca1e05fd7
Mj <mj@Mjui-MacBookPro.local>
2025-07-27 02:19:23 +0900

    agreement added

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
+/*import { Authenticator } from '@aws-amplify/ui-react';
+*/
 import AppWrapper from "./App.tsx"; // AppWrapper를 불러오도록 변경
 import "./index.css";
 import { Amplify } from "aws-amplify";
